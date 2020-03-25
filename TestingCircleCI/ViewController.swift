54d54d09dc54d61e9188cfee12e5fbcd362b5b63
Martin Stojcev <martinstojcev@Martins-MacBook-Pro.local>
2020-03-25 09:28:24 +0100

    Initial commit

---
+++ TestingCircleCI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TestingCircleCI
+//
+//  Created by Martin Stojcev on 3/25/20.
+//  Copyright © 2020 Martin Stojcev. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
